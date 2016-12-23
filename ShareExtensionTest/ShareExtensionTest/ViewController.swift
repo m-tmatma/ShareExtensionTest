bc10024a7f3c27bb8b3fef81a2c6a4b4977f84f9
Masaru Tsuchiyama <m.tmatma@gmail.com>
2016-12-23 10:55:59 +0900

    Share Extension の元となるプロジェクトを追加

---
+++ ShareExtensionTest/ShareExtensionTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareExtensionTest
+//
+//  Created by tsuchiyamamasaru on 2016/12/23.
+//  Copyright © 2016年 tsuchiyamamasaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
